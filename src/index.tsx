4a68179e77122f6d4916ad62430ce345b5037fe1
Sam Thompson <sam.thompson.work@gmail.com>
2020-08-22 13:25:54 +0100

    [ Tasks ] Import dummy task json

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { render } from "react-dom";
+import tasklist from "./_data/tasks.json";
 
-const Application: React.FC<{}> = () => <h1>Test</h1>;
+const Application: React.FC = () => {
+  console.log(tasklist);
 
+  return <h1>Test</h1>;
+};
 render(<Application />, document.getElementById("root"));
